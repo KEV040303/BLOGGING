dc6986d61103de6b6b87c3a567f6f2186f30c1fd
KEV040303 <kevinandresyanezmail.com>
2024-05-03 05:10:18 -0500

    Add existing file

--- src/controller.js
+++ src/controller.js
@@ -21,10 +21,14 @@ export const createUser = async (req, res) => {
 export const updateUser = async (req, res) => {
   try {
     const { id } = req.params
-    const { nombre, email, contraseña, usuario, roleId } = req.body
+    const { nombre, email, contraseña, usuario, roleId, userId } = req.body
 
     if (!id) {
       return res.status(400).json({ message: 'ID de usuario no proporcionado' })
+    }
+
+    if (id !== userId) {
+      return res.status(403).json({ message: 'No tienes permiso para actualizar este usuario' })
     }
 
     if (!nombre && !email && !contraseña && !usuario && !roleId) {
@@ -92,9 +96,14 @@ export const updateUser = async (req, res) => {
 export const deleteUser = async (req, res) => {
   try {
     const { id } = req.params
+    const { userId } = req.body
 
     if (!id) {
       return res.status(400).json({ message: 'ID de usuario no proporcionado' })
+    }
+
+    if (id !== userId) {
+      return res.status(403).json({ message: 'No tienes permiso para eliminar este usuario' })
     }
 
     const [currentUser] = await pool.query('SELECT * FROM usuarios WHERE id = ?', [id])
@@ -226,5 +235,310 @@ export const updatePost = async (req, res) => {
 }
 
 export const deletePost = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { usuarioId } = req.body
 
+    if (!id) {
+      return res.status(400).json({ message: 'ID de publicación no proporcionado' })
+    }
+
+    // Verificar si el usuario tiene permiso para eliminar la publicación
+    const [existingPost] = await pool.query('SELECT * FROM publicaciones WHERE id = ?', [id])
+
+    if (!existingPost.length) {
+      return res.status(404).json({ message: 'Publicación no encontrada' })
+    }
+
+    if (existingPost[0].usuario_id !== usuarioId) {
+      return res.status(403).json({ message: 'No tienes permiso para eliminar esta publicación' })
+    }
+
+    // Eliminar las filas en publicaciones_categorias que hacen referencia a la publicación que se va a eliminar
+    await pool.execute('DELETE FROM publicaciones_categorias WHERE publicacion_id = ?', [id])
+
+    // Luego eliminar la publicación
+    await pool.execute('DELETE FROM publicaciones WHERE id = ?', [id])
+
+    res.status(200).json({ message: 'Publicación eliminada exitosamente' })
+  } catch (err) {
+    console.error('Error al eliminar la publicación:', err)
+    return res.status(500).json({ message: 'Error al eliminar la publicación' })
+  }
+}
+
+export const getPosts = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    if (!id) {
+      return res.status(400).json({ message: 'ID de usuario no proporcionado' })
+    }
+
+    const query = 'SELECT * FROM publicaciones WHERE usuario_id = ?'
+
+    const [posts] = await pool.query(query, [id])
+
+    res.status(200).json({ posts })
+  } catch (err) {
+    console.error('Error al obtener las publicaciones:', err)
+
+    res.status(500).json({ message: 'Error al obtener las publicaciones' })
+  }
+}
+
+export const getPostCategories = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    if (!id) {
+      return res.status(400).json({ message: 'ID de categoría no proporcionado' })
+    }
+
+    const query = `
+        SELECT pc.publicacion_id, p.titulo, p.contenido, p.usuario_id, p.nombre_usuario 
+        FROM publicaciones_categorias pc
+        JOIN publicaciones p ON pc.publicacion_id = p.id
+        WHERE pc.categoria_id = ?
+      `
+
+    const [posts] = await pool.query(query, [id])
+
+    res.status(200).json({ posts })
+  } catch (err) {
+    console.error('Error al obtener las categorías de las publicaciones:', err)
+
+    res.status(500).json({ message: 'Error al obtener las categorías de las publicaciones' })
+  }
+}
+
+export const getPostTitle = async (req, res) => {
+  try {
+    const { title } = req.params
+
+    if (!title) {
+      return res.status(400).json({ message: 'Debes proporcionar un título para buscar' })
+    }
+
+    const query = `
+        SELECT * FROM publicaciones 
+        WHERE titulo LIKE ?
+      `
+
+    const [posts] = await pool.query(query, [`%${title}%`])
+
+    res.status(200).json({ posts })
+  } catch (err) {
+    console.error('Error al obtener las publicaciones por título:', err)
+
+    res.status(500).json({ message: 'Error al obtener las publicaciones por título' })
+  }
+}
+
+export const getCategory = async (req, res) => {
+  try {
+    const { roleId } = req.body
+
+    if (roleId !== 1) {
+      return res.status(403).json({ message: 'No tienes permiso para acceder a esta función' })
+    }
+
+    const [categories] = await pool.query('SELECT * FROM categorias')
+
+    res.status(200).json({ categories })
+  } catch (err) {
+    console.error('Error al obtener las categorías:', err)
+
+    res.status(500).json({ message: 'Error al obtener las categorías' })
+  }
+}
+
+export const createCategory = async (req, res) => {
+  try {
+    const { roleId, nombre } = req.body
+
+    if (roleId !== 1) {
+      return res.status(403).json({ message: 'No tienes permiso para acceder a esta función' })
+    }
+
+    if (!nombre) {
+      return res.status(400).json({ message: 'El nombre de la categoría es obligatorio' })
+    }
+
+    const [existingCategory] = await pool.query('SELECT * FROM categorias WHERE nombre = ?', [nombre])
+
+    if (existingCategory.length) {
+      return res.status(400).json({ message: 'La categoría ya existe' })
+    }
+
+    await pool.execute('INSERT INTO categorias (nombre) VALUES (?)', [nombre])
+
+    res.status(201).json({ message: 'Categoría creada exitosamente' })
+  } catch (err) {
+    console.error('Error al crear la categoría:', err)
+    res.status(500).json({ message: 'Error al crear la categoría' })
+  }
+}
+
+export const updateCategory = async (req, res) => {
+  try {
+    const { roleId, nombre } = req.body
+    const { id } = req.params
+
+    if (roleId !== 1) {
+      return res.status(403).json({ message: 'No tienes permiso para acceder a esta función' })
+    }
+
+    if (!id) {
+      return res.status(400).json({ message: 'ID de categoría no proporcionado' })
+    }
+
+    if (!nombre) {
+      return res.status(400).json({ message: 'El nombre de la categoría es obligatorio' })
+    }
+
+    const [existingCategory] = await pool.query('SELECT * FROM categorias WHERE id = ?', [id])
+
+    if (!existingCategory.length) {
+      return res.status(404).json({ message: 'Categoría no encontrada' })
+    }
+
+    await pool.execute('UPDATE categorias SET nombre = ? WHERE id = ?', [nombre, id])
+
+    res.status(200).json({ message: 'Categoría actualizada exitosamente' })
+  } catch (err) {
+    console.error('Error al actualizar la categoría:', err)
+
+    res.status(500).json({ message: 'Error al actualizar la categoría' })
+  }
+}
+
+export const deleteCategory = async (req, res) => {
+  try {
+    const { roleId } = req.body
+    const { id } = req.params
+
+    if (roleId !== 1) {
+      return res.status(403).json({ message: 'No tienes permiso para acceder a esta función' })
+    }
+
+    if (!id) {
+      return res.status(400).json({ message: 'ID de categoría no proporcionado' })
+    }
+
+    const [existingCategory] = await pool.query('SELECT * FROM categorias WHERE id = ?', [id])
+
+    if (!existingCategory.length) {
+      return res.status(404).json({ message: 'Categoría no encontrada' })
+    }
+
+    await pool.execute('DELETE FROM categorias WHERE id = ?', [id])
+
+    res.status(200).json({ message: 'Categoría eliminada exitosamente' })
+  } catch (err) {
+    console.error('Error al eliminar la categoría:', err)
+    res.status(500).json({ message: 'Error al eliminar la categoría' })
+  }
+}
+
+export const createComment = async (req, res) => {
+  try {
+    const { id, comentario, usuarioId } = req.body
+
+    if (!id || !comentario || !usuarioId) {
+      return res.status(400).json({ message: 'Todos los campos son obligatorios' })
+    }
+
+    const [existingPost] = await pool.query('SELECT * FROM publicaciones WHERE id = ?', [id])
+    if (!existingPost.length) {
+      return res.status(404).json({ message: 'La publicación no existe' })
+    }
+
+    await pool.execute('INSERT INTO comentarios (publicacion_id, comentario, usuario_id) VALUES (?, ?, ?)', [id, comentario, usuarioId])
+
+    res.status(201).json({ message: 'Comentario creado exitosamente' })
+  } catch (err) {
+    console.error('Error al crear el comentario:', err)
+
+    return res.status(500).json({ message: 'Error al crear el comentario' })
+  }
+}
+
+export const updateComments = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { comentario, userId } = req.body
+
+    if (!id) {
+      return res.status(400).json({ message: 'ID de comentario no proporcionado' })
+    }
+
+    const [existingComment] = await pool.query('SELECT * FROM comentarios WHERE id = ?', [id])
+    if (!existingComment.length) {
+      return res.status(404).json({ message: 'Comentario no encontrado' })
+    }
+
+    if (existingComment[0].usuario_id !== userId) {
+      return res.status(403).json({ message: 'No tienes permiso para actualizar este comentario' })
+    }
+
+    await pool.execute('UPDATE comentarios SET comentario = ? WHERE id = ?', [comentario, id])
+
+    res.status(200).json({ message: 'Comentario actualizado exitosamente' })
+  } catch (err) {
+    console.error('Error al actualizar el comentario:', err)
+    return res.status(500).json({ message: 'Error al actualizar el comentario' })
+  }
+}
+
+export const deleteComment = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { userId } = req.body
+
+    if (!id) {
+      return res.status(400).json({ message: 'ID de comentario no proporcionado' })
+    }
+
+    const [existingComment] = await pool.query('SELECT * FROM comentarios WHERE id = ?', [id])
+
+    if (!existingComment.length) {
+      return res.status(404).json({ message: 'Comentario no encontrado' })
+    }
+
+    if (existingComment[0].usuario_id !== userId) {
+      return res.status(403).json({ message: 'No tienes permiso para eliminar este comentario' })
+    }
+
+    await pool.execute('DELETE FROM comentarios WHERE id = ?', [id])
+
+    res.status(200).json({ message: 'Comentario eliminado exitosamente' })
+  } catch (err) {
+    console.error('Error al eliminar el comentario:', err)
+    return res.status(500).json({ message: 'Error al eliminar el comentario' })
+  }
+}
+
+export const getCommentsByPost = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    if (!id) {
+      return res.status(400).json({ message: 'ID de publicación no proporcionado' })
+    }
+
+    const query = `
+        SELECT c.id, c.comentario, c.usuario_id, u.nombre AS nombre_usuario
+        FROM comentarios c
+        INNER JOIN usuarios u ON c.usuario_id = u.id
+        WHERE c.publicacion_id = ?
+      `
+
+    const [comments] = await pool.query(query, [id])
+
+    res.status(200).json({ comments })
+  } catch (err) {
+    console.error('Error al obtener los comentarios de la publicación:', err)
+    res.status(500).json({ message: 'Error al obtener los comentarios de la publicación' })
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { PORT } from './config/config.js'
-import { createUser, updateUser, deleteUser, getUsers, createPost, updatePost } from './controller.js'
+import { createUser, updateUser, deleteUser, getUsers, createPost, updatePost, deletePost, getPosts, getPostCategories, getPostTitle, getCategory, createCategory, updateCategory, deleteCategory, createComment, updateComments, deleteComment, getCommentsByPost } from './controller.js'
 
 const app = express()
 
 app.use(express.json())
 
+// USER
 // Ruta para crear el nuevo usuario
 app.post('/user', createUser)
 // Ruta para actualizar el usuario
@@ -15,12 +16,37 @@ app.delete('/user/:id', deleteUser)
 // Ruta para obtener todos los usuarios
 app.get('/users', getUsers)
 
+// POST
 // Ruta para crear una publicacion
 app.post('/post', createPost)
 // Ruta para actualizar una publicacion
 app.put('/post/:id', updatePost)
 // Ruta para eliminar una publicacion
-app.delete('/post/:id', deleteUser)
+app.delete('/post/:id', deletePost)
+// Ruta para ver publicaciones propias y de otros
+app.get('/post/:id', getPosts)
+// Ruta para ver publicaciones por categorias
+app.get('/post/category/:id', getPostCategories)
+// Ruta para ver publicaciones por titulo
+app.get('/post/title/:title', getPostTitle)
+
+// Ruta del admin para ver las categorias
+app.get('/category', getCategory)
+// Ruta del admin para crear una categoria
+app.post('/category', createCategory)
+// Ruta del admin para editar una categoria
+app.put('/category/:id', updateCategory)
+// Ruta del admin para eliminar una categoria
+app.delete('/category/:id', deleteCategory)
+
+// Ruta para crear un comentario
+app.post('/comment', createComment)
+// Ryta para editar un comentario
+app.put('/comment/:id', updateComments)
+// Ruta para eliminar un comentario
+app.delete('/comment/:id', deleteComment)
+// Ruta para obtener los comentarios de cada publicacion
+app.get('/comment/:id', getCommentsByPost)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
